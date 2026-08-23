c7e914ae223b27507ac844412d588d3063f39374
AlejandroFerreira1 <alejandro19812007@gmail.com>
2026-08-23 16:55:32 -0300

    fix: correção do sistema de copia de senha

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const btnEL = document.querySelector(".btn");
+const btnEL = document.querySelector(".btn")
+const inputEL = document.querySelector("#input")
+const copyIconEL = document.querySelector(".fa-copy")
 
 btnEL.addEventListener("click", () => {
-    createPassword();
-});
+    createPassword()
+})
+
+copyIconEL.addEventListener("click", ()=>{
+    copyPassword()
+})
 
 function createPassword() {
-    const chars = "0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const passwordLength = 16;
+    const chars = "0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const passwordLength = 14
     let password = ""
-    for (let i = 0; i < passwordLength; i++) {
-        const randomNum = Math.floor(Math.random() * chars.length);
-        password += chars.substring(randomNum, randomNum + 1);
-        console.log(randomNum, password);
+    
+    for (let index = 0; index < passwordLength; index++) {
+        const randomNum = Math.floor(Math.random() * chars.length)
+        password += chars.substring(randomNum, randomNum + 1)
     }
+    inputEL.value = password
+}
+
+function copyPassword() {
+    inputEL.select()
+    inputEL.setSelectionRange(0,9999)
+    navigator.clipboard.writeText(inputEL.value)
 }
