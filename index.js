fea7e8c4c7edcc38f3bb03c7dd21f9198968dd41
AlejandroFerreira1 <alejandro19812007@gmail.com>
2026-08-23 20:28:41 -0300

    finalização do projeto de gerador de senhas aleatorias

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const btnEL = document.querySelector(".btn")
-const inputEL = document.querySelector("#input")
-const copyIconEL = document.querySelector(".fa-copy")
+const btnEL = document.querySelector(".btn");
+const inputEL = document.querySelector("#input");
+const copyIconEL = document.querySelector(".fa-copy");
+const alertContainerEL = document.querySelector(".alert-container");
 
 btnEL.addEventListener("click", () => {
-    createPassword()
-})
+    createPassword();
+});
 
 copyIconEL.addEventListener("click", ()=>{
-    copyPassword()
-})
+    copyPassword();
+    if(inputEL.value){
+        alertContainerEL.classList.remove("active");
+    setTimeout(() => {
+        alertContainerEL.classList.add("active");
+    }, 2000);
+    }
+    
+});
 
 function createPassword() {
     const chars = "0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -16,14 +24,15 @@ function createPassword() {
     let password = ""
     
     for (let index = 0; index < passwordLength; index++) {
-        const randomNum = Math.floor(Math.random() * chars.length)
-        password += chars.substring(randomNum, randomNum + 1)
+        const randomNum = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNum, randomNum + 1);
     }
-    inputEL.value = password
+    inputEL.value = password; 
+    alertContainerEL.innerText = password + " copiado!";
 }
 
 function copyPassword() {
-    inputEL.select()
-    inputEL.setSelectionRange(0,9999)
-    navigator.clipboard.writeText(inputEL.value)
+    inputEL.select();
+    inputEL.setSelectionRange(0,9999);
+    navigator.clipboard.writeText(inputEL.value);
 }
